c80e8afb9df435abaf92759c1769a760b5d8eb56
CyberCaban <andrewgamer63@gmail.com>
2024-05-28 23:58:28 +0300

    debloated 2

--- main.c
+++ main.c
@@ -3,36 +3,6 @@
 
 x, y, cy, cx, dx, dy, nbors, front[ROWS][COLS], back[ROWS][COLS];
 
-void display() {
-  for (x = 0; x < ROWS; ++x) {
-    for (y = 0; y < COLS; ++y) {
-      printf("%c", ".#"[front[x][y]]);
-    }
-    printf("\n");
-  }
-}
-
-mod(a, b) { return (a % b + b) % b; }
-
-void update() {
-  for (cy = 0; cy < ROWS; ++cy) {
-    for (cx = 0; cx < COLS; ++cx) {
-      nbors = 0;
-      for (dx = -1; dx <= 1; ++dx) {
-        for (dy = -1; dy <= 1; ++dy) {
-          if (dx != 0 || dy != 0) {
-            x = ((cx + dx) % COLS + COLS) % COLS;
-            y = ((cy + dy) % ROWS + ROWS) % ROWS;
-            if (front[y][x])
-              nbors += 1;
-          }
-        }
-      }
-      back[cy][cx] = front[cy][cx] ? (nbors == 2 || nbors == 3) : nbors == 3;
-    }
-  }
-}
-
 main() {
   // glider
   front[0][1] = 1;
@@ -42,8 +12,30 @@ main() {
   front[2][2] = 1;
 
   for (;;) {
-    display();
-    update();
+    // for (x = 0; x < ROWS; ++x) {
+    //   for (y = 0; y < COLS; ++y) {
+    //     printf("%c", ".#"[front[x][y]]);
+    //   }
+    //   printf("\n");
+    // }
+    for (cy = 0; cy < ROWS; ++cy) {
+      for (cx = 0; cx < COLS; ++cx) {
+        printf("%c", ".#"[front[cy][cx]]);
+        nbors = 0;
+        for (dx = -1; dx <= 1; ++dx) {
+          for (dy = -1; dy <= 1; ++dy) {
+            if (dx != 0 || dy != 0) {
+              x = ((cx + dx) % COLS + COLS) % COLS;
+              y = ((cy + dy) % ROWS + ROWS) % ROWS;
+              if (front[y][x])
+                nbors += 1;
+            }
+          }
+        }
+        back[cy][cx] = front[cy][cx] ? (nbors == 2 || nbors == 3) : nbors == 3;
+      }
+      printf("\n");
+    }
     memcpy(front, back, sizeof(front));
     printf("\033[%dA\033[%dD", ROWS, COLS);
     usleep(100 * 1000);
