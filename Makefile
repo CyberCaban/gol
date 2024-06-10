7cce8579e81fa8c299fcb68dd461af95cc249af3
CyberCaban <andrewgamer63@gmail.com>
2024-06-10 21:14:44 +0300

    added input range element

--- Makefile
+++ Makefile
@@ -1,9 +1,11 @@
 default: 
 	gcc main.c
 
-window: 
-	gcc window.c -lraylib -lgdi32 -lwinmm -o w
-
-exec:
+window:
 	gcc -I ./lib -c window.c
 	gcc -L ./lib -o w.exe window.o -lraylib -lgdi32 -lwinmm
+
+exec: 
+	gcc -I ./lib -c window.c
+	gcc -L ./lib -o w.exe window.o -lraylib -lgdi32 -lwinmm
+	./w.exe
--- window.c
+++ window.c
@@ -2,31 +2,84 @@
 #include "raymath.h"
 #include "rlgl.h"
 
-#define WIDTH 640
-#define HEIGHT 480
+#define WIDTH 1280
+#define HEIGHT 720
+
+typedef struct {
+    int centerX;
+    int centerY;
+    float radius;
+} Circle;
+
+void DrawCircleCircle(Circle circle, Color color){
+    DrawCircle(circle.centerX, circle.centerY, circle.radius, color);
+}
+
+void InputRange(double *state, int posX, int posY, double width, int min, int max){
+    static int focus = 0;
+    const double height = 5;
+    const double offset = 30;
+    double circlePosX;
+
+    if(*state < min) 
+        circlePosX = posX + 1;
+    else if(*state > max) 
+        circlePosX = posX + width - 1;
+    else 
+        circlePosX = (*state - min) / (max - min) * width - 1 + posX;
+        
+    Rectangle rect = {posX, posY, width, height};
+    Circle circle = {circlePosX, posY + height / 2, height * 2};
+
+    DrawRectangleRec(rect, BLACK);
+    DrawCircleCircle(circle, LIGHTGRAY);
+
+    double newState = (GetMouseX() - posX + 1) / width * (max - min) + min;
+
+    if(IsMouseButtonDown(MOUSE_LEFT_BUTTON)){
+        if(CheckCollisionPointRec(GetMousePosition(), 
+            (Rectangle){ 
+                .width = rect.width, 
+                .height = rect.height + offset, 
+                .x = rect.x, 
+                .y = rect.y - offset / 2
+            }))
+        {
+            focus = 1;
+        }
+    } else {
+        focus = 0;
+    }
+
+    if(focus){
+        if (newState < min) *state = min;
+        else if (newState > max) *state = max;
+        else *state = newState;
+    }
+}
 
 int main()
 {
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+    // SetConfigFlags(FLAG_WINDOW_UNDECORATED);
     InitWindow(WIDTH, HEIGHT, "hello world");
 
     Camera2D camera = {0, .zoom = 1.0f};
     SetTargetFPS(120);
-     
+
+    double state = 120;
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
-        BeginMode2D(camera);
-        ClearBackground(RAYWHITE);
-        DrawCircle(GetMouseX(), GetMouseY(), 40, MAROON);
-        rlPushMatrix();
-        Rectangle rec = {100, 100, 100, 100};
-        rlRotatef(45.0f, 0.0f, 0.0f, 1.0f);
-        DrawRectangle(rec.x, rec.y, rec.width, rec.height, BLUE);
-        // rlTranslatef(GetScreenWidth()/2, GetScreenHeight()/2, 0.0f);
-        rlPopMatrix();
-
-        EndMode2D();
+            BeginMode2D(camera);
+                ClearBackground(RAYWHITE);
+                DrawCircle(GetMouseX(), GetMouseY(), 40, MAROON);
+                DrawText("state: ", 10, 10, 20, BLACK);
+                DrawText(TextFormat("%d", (int)state), 80, 10, 20, BLACK);
+                InputRange(&state, 10, 50, 150, -10, 27);
+                
+            EndMode2D();
         EndDrawing();
     }
 
